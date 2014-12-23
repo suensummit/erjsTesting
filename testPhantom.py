fb7994de9b285ee7f9d6369fbcf9865d2b9e4798
Summit Suen <summit.suen@gmail.com>
2014-12-23 14:23:32 +0800

    update python codes

--- testPhantom.py
+++ testPhantom.py
@@ -1,19 +1,19 @@
 # test code: selenium plus phantomjs
 
-from selenium import webdriver
-driver = webdriver.PhantomJS()
-driver.set_window_size(1120, 550)
-driver.get("https://duckduckgo.com/")
-driver.find_element_by_id('search_form_input_homepage').send_keys("realpython")
-driver.find_element_by_id("search_button_homepage").click()
-print driver.current_url
-driver.quit()
-
-# test code 2
-
 # from selenium import webdriver
-# driver = webdriver.Firefox()
+# driver = webdriver.PhantomJS()
+# driver.set_window_size(1120, 550)
 # driver.get("https://duckduckgo.com/")
 # driver.find_element_by_id('search_form_input_homepage').send_keys("realpython")
 # driver.find_element_by_id("search_button_homepage").click()
+# print driver.current_url
 # driver.quit()
+
+# test code 2
+
+from selenium import webdriver
+driver = webdriver.Firefox()
+driver.get("https://duckduckgo.com/")
+driver.find_element_by_id('search_form_input_homepage').send_keys("realpython")
+driver.find_element_by_id("search_button_homepage").click()
+driver.quit()
