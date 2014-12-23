4b6494c2db63f4a3925316c062a1533e027e1b8e
Summit Suen <summit.suen@gmail.com>
2014-12-23 13:13:00 +0800

    update python codes

--- testPhantom.py
+++ testPhantom.py
@@ -8,3 +8,12 @@ driver.find_element_by_id('search_form_input_homepage').send_keys("realpython")
 driver.find_element_by_id("search_button_homepage").click()
 print driver.current_url
 driver.quit()
+
+# test code 2
+
+# from selenium import webdriver
+# driver = webdriver.Firefox()
+# driver.get("https://duckduckgo.com/")
+# driver.find_element_by_id('search_form_input_homepage').send_keys("realpython")
+# driver.find_element_by_id("search_button_homepage").click()
+# driver.quit()
