d8c5ed95f9fbd1c8531ab3c5c7961c979080713c
Summit Suen <summit.suen@gmail.com>
2015-01-06 17:14:02 +0800

    update web_test.py

--- web_test.py
+++ web_test.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 import csv
-import time
 
 # set test sample ec site
 #url = "https://"
@@ -8,22 +7,15 @@ url = "https://duckduckgo.com/"
 
 #for x in testbot:
 #	for action in testbot(x).act_list:
-driver = webdriver.PhantomJS()
-driver.get(url)
-time.sleep(3)
-driver.set_window_size(1120, 550)
-driver2 = webdriver.Firefox()
+driver1 = webdriver.PhantomJS()
+driver1.get(url)
+driver2 = webdriver.PhantomJS()
 driver2.get(url)
-time.sleep(3)
-driver3 = webdriver.Firefox()
+driver3 = webdriver.PhantomJS()
 driver3.get(url)
-time.sleep(3)
-driver3.set_window_size(1120, 550)
 #driver.get(url)
 #driver.find_element_by_id().send_keys()
 #driver.find_element_by_id().click()
-driver.quit()
-time.sleep(3)
+driver1.quit()
 driver2.quit()
-time.sleep(3)
 driver3.quit()
