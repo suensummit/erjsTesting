06a39bc671c3f203edf93891138bdac0c48ae47b
Summit Suen <summit.suen@gmail.com>
2015-01-07 18:20:24 +0800

    dynamically generating drivers

--- web_test.py
+++ web_test.py
@@ -1,25 +1,28 @@
 from selenium import webdriver
 import csv
+import time
 
-# set test sample ec site
-#url = "https://"
-url = "https://duckduckgo.com/"
+# load test data
+
+# set test sample ec url
+url = "https://suensummit.github.io/erjsTesting/"
 
 # 
 driver = {}
 k = 0
-while k < :
+while k <= len(ssid):
     key = k
     value = webdriver.PhantomJS()
     driver[key] = value
     k += 1
 
-for x in testbot:
+for x in range(len(ssid)):
 	for action in testbot(x).act_list:
 		driver[x].get(url)
+		time.sleep(1)
+		driver[x].find_element_by_id().click()
+		time.sleep(1)
 		driver[x].quit()
-
-
 
 #driver.find_element_by_id().send_keys()
 #driver.find_element_by_id().click()
