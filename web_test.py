01dd8b70cdbe3d588b0e5913a3fa1ad5fbbc7408
Summit Suen <summit.suen@gmail.com>
2015-01-07 17:36:54 +0800

    dynamically generating drivers

--- web_test.py
+++ web_test.py
@@ -5,31 +5,21 @@ import csv
 #url = "https://"
 url = "https://duckduckgo.com/"
 
-#for x in testbot:
-#	for action in testbot(x).act_list:
-a = {}
+# 
+driver = {}
 k = 0
-while k < 3:
+while k < :
     key = k
-    value = webdriver.Firefox()
-    a[key] = value 
+    value = webdriver.PhantomJS()
+    driver[key] = value
     k += 1
 
-#driver + i = webdriver.Firefox()
-#driver1 = webdriver.PhantomJS()
-a[0].get(url)
-a[1].get(url)
-a[2].get(url)
-#driver2 = webdriver.PhantomJS()
-#driver2.get(url)
-#driver3 = webdriver.PhantomJS()
-#driver3.get(url)
-#driver.get(url)
+for x in testbot:
+	for action in testbot(x).act_list:
+		driver[x].get(url)
+		driver[x].quit()
+
+
+
 #driver.find_element_by_id().send_keys()
 #driver.find_element_by_id().click()
-a[0].quit()
-a[1].quit()
-a[2].quit()
-#driver1.quit()
-#driver2.quit()
-#driver3.quit()
