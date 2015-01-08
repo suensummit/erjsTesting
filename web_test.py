0e033a8185912bce26dd18fa1ac198eac47f1891
Summit Suen <summit.suen@gmail.com>
2015-01-08 18:23:27 +0800

    update test python codes

--- web_test.py
+++ web_test.py
@@ -1,27 +1,33 @@
 from selenium import webdriver
+from 
 import csv
 import time
 
 # load test data
+with open('web_test_funnel.csv', 'rb') as f:
+    reader = csv.reader(f)
+    testbot_raw = list(reader)
+
+testbot = sorted(testbot_raw, key=lambda testbot_raw: testbot_raw[2])
 
 # set test sample ec url
-url = "https://suensummit.github.io/erjsTesting/"
+url = "file:///Users/summitsuen/Documents/erjsTesting/index.html"
+#url = "https://suensummit.github.io/erjsTesting/"
 
 # 
 driver = {}
 k = 0
 while k <= len(ssid):
-    key = k
+    key = ssid[k]
     value = webdriver.PhantomJS()
     driver[key] = value
     k += 1
 
-for x in range(len(ssid)):
+#for x in range(len(ssid)):
+for x in ssid:
 	for action in testbot(x).act_list:
 		driver[x].get(url)
-		time.sleep(1)
 		driver[x].find_element_by_id().click()
-		time.sleep(1)
 		driver[x].quit()
 
 #driver.find_element_by_id().send_keys()
