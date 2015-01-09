703063b3359411823ccdb33c4a72df880729e643
Summit Suen <summit.suen@gmail.com>
2015-01-09 18:34:25 +0800

    update py

---
+++ testServer2.py
@@ -0,0 +1,13 @@
+class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+        logging.error(self.headers)
+        # whatever else you would like to log here
+        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+Handler = ServerHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "serving at port", PORT
+httpd.serve_forever()
