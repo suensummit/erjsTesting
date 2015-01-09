8a20c956c02caa74dbf7c79e9e714e420479dcef
Summit Suen <summit.suen@gmail.com>
2015-01-09 16:34:16 +0800

    update py

--- web_test.py
+++ web_test.py
@@ -21,8 +21,8 @@ k = 0
 ssid = df.ssid.unique()
 while k < len(ssid)-1:
 	key = ssid[k]
-	#value = webdriver.PhantomJS()
-	value = webdriver.Firefox()
+	value = webdriver.PhantomJS()
+	#value = webdriver.Firefox()
 	driver[key] = value
 	k += 1
 
